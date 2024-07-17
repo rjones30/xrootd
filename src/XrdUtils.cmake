4d9167934c02c24577d99cddae9b318cbbe92378
Richard Jones <rjones30@gmail.com>
2024-07-17 11:42:21 -0400

    * fix a typo in the last commit [rtj]

--- src/XrdUtils.cmake
+++ src/XrdUtils.cmake
@@ -307,7 +307,7 @@ add_library(
 )
 
 if (NOT "${OPENSSL_INCLUDE_DIR}" STREQUAL "")
-    target_include_directories(XrdUtils_static PRIVATE ${OPENSSL_INCLUDE_DIR))
+    target_include_directories(XrdUtils_static PRIVATE ${OPENSSL_INCLUDE_DIR})
 endif()
 
 target_link_libraries(
